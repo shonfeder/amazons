ffcb5a352d0bb1ce80cd3bf0eeca06e5bb986e7c
Shon Feder <shon.feder@key.me>
2017-12-28 23:05:17 -0500

    Abbreviations are used for common submodules while testing

--- lib/test/test_game.ml
+++ lib/test/test_game.ml
@@ -2,6 +2,10 @@ open Core
 open QCheck
 open Game
 
+module Pc = Piece
+module Sq = Square
+module Bd = Board
+
 exception Testing
 
 let check
@@ -16,15 +20,15 @@ module GameGen = struct
 
   module Piece = struct
     let kind
-      : Piece.kind Gen.t
-      = Gen.oneofl [Piece.Amazon; Piece.Arrow]
+      : Pc.kind Gen.t
+      = Gen.oneofl [Pc.Amazon; Pc.Arrow]
 
     let color
-      : Piece.color Gen.t
-      = Gen.oneofl [Piece.Black; Piece.White]
+      : Pc.color Gen.t
+      = Gen.oneofl [Pc.Black; Pc.White]
 
     let t
-      : Piece.t Gen.t
+      : Pc.t Gen.t
       = fun rand ->
         { color = color rand
         ; kind  = kind rand }
@@ -32,7 +36,7 @@ module GameGen = struct
 
   module Square = struct
     let t
-      : Square.t Gen.t
+      : Sq.t Gen.t
       = fun rand ->
         { coord = coord rand
         ; piece = Gen.opt Piece.t rand }
@@ -42,20 +46,18 @@ module GameGen = struct
   struct
 
     let place_piece_on_sq
-      : Game.Piece.t -> Game.Square.t -> Game.Square.t
-      = let open Game in
-      fun piece Square.{coord} -> Square.make coord piece
+      : Pc.t -> Sq.t -> Sq.t
+      = fun piece Sq.{coord} -> Sq.make coord piece
 
     let amazons =
-      let open Game in
-      List.init 4 (fun _ -> Piece.(make Black Amazon)) @
-      List.init 4 (fun _ -> Piece.(make White Amazon))
+      List.init 4 (fun _ -> Pc.(make Black Amazon)) @
+      List.init 4 (fun _ -> Pc.(make White Amazon))
 
     let t
-      : Board.t Gen.t
+      : Bd.t Gen.t
       = fun rand ->
         let num_arrows = Gen.(0 -- 45) rand in
-        let squares = Gen.shuffle_l Board.empty rand in
+        let squares = Gen.shuffle_l Bd.empty rand in
         let (amazon_sqs, rest)  = List.split_n squares 8 in
         let (blk_arr_sqs, rest) = List.split_n rest num_arrows in
         let (wht_arr_sqs, rest) = List.split_n rest num_arrows in
@@ -63,10 +65,10 @@ module GameGen = struct
           List.map2_exn ~f:place_piece_on_sq amazons amazon_sqs
         in
         let blk_arr_sqs =
-          List.map ~f:(place_piece_on_sq Game.Piece.(black Arrow)) blk_arr_sqs
+          List.map ~f:(place_piece_on_sq Pc.(black Arrow)) blk_arr_sqs
         in
         let wht_arr_sqs =
-          List.map ~f:(place_piece_on_sq Game.Piece.(white Arrow)) wht_arr_sqs
+          List.map ~f:(place_piece_on_sq Pc.(white Arrow)) wht_arr_sqs
         in
         Gen.shuffle_l (amazon_sqs @ blk_arr_sqs @ wht_arr_sqs @ rest) rand
   end
@@ -77,35 +79,35 @@ module Arbitrary = struct
   let coord = pair (0 -- 9) (0 -- 9)
   module Piece = struct
     let kind
-      : Piece.kind arbitrary
-      = make ~print:Piece.show_kind GameGen.Piece.kind
+      : Pc.kind arbitrary
+      = make ~print:Pc.show_kind GameGen.Piece.kind
 
     let color
-      : Piece.color arbitrary
-      = make ~print:Piece.show_color GameGen.Piece.color
+      : Pc.color arbitrary
+      = make ~print:Pc.show_color GameGen.Piece.color
 
     let t
-      : Piece.t arbitrary
-      = make ~print:Piece.show GameGen.Piece.t
+      : Pc.t arbitrary
+      = make ~print:Pc.show GameGen.Piece.t
   end
 
   module Square = struct
     let t
-      : Square.t arbitrary
-      = make ~print:Square.show GameGen.Square.t
+      : Sq.t arbitrary
+      = make ~print:Sq.show GameGen.Square.t
   end
 
   module Board = struct
     let t
-      : Board.t arbitrary
-      = make ~print:Board.show GameGen.Board.t
+      : Bd.t arbitrary
+      = make ~print:Bd.show GameGen.Board.t
   end
 
 end
 
 module Aux = struct
   let place_exn coord piece board =
-    match Board.(place coord piece empty) with
+    match Bd.(place coord piece empty) with
     | Error _ -> raise Testing (* This should be impossible *)
     | Ok board -> board
 
@@ -117,7 +119,7 @@ module Aux = struct
     1 = List.length @@ Caml.List.sort_uniq compare ls
 
   let are_in_a_line sqs =
-    let coords = List.map ~f:Square.coord sqs in
+    let coords = List.map ~f:Sq.coord sqs in
     let (xs, ys) = List.unzip coords in
     coords = (List.sort coords ~cmp:compare) &&
     (is_ascending_sequence xs || is_single_value xs) &&
@@ -130,7 +132,7 @@ let tests = [
     ~name:"reading coords"
     Arbitrary.coord
     begin fun coord ->
-      let coord_read = Game.read_coord @@ Game.show_coord coord in
+      let coord_read = read_coord @@ show_coord coord in
       coord_read = coord
     end
   ;
@@ -138,49 +140,45 @@ let tests = [
     ~name:"possible kinds of a piece"
     Arbitrary.Piece.kind
     begin fun kind ->
-      Piece.(kind = Amazon || kind = Arrow)
+      Pc.(kind = Amazon || kind = Arrow)
     end
   ;
   Test.make
     ~name:"possible colors of a piece"
     Arbitrary.Piece.color
     begin fun color ->
-      Piece.(color = Black || color = White)
+      Pc.(color = Black || color = White)
     end
   ;
   Test.make
     ~name:"possible pieces"
     Arbitrary.Piece.t
-    begin let open Piece in
-      fun {color; kind} ->
-        (color = Black || color = White)
-        &&
-        (kind = Amazon || kind = Arrow)
+    begin fun Pc.{color; kind} ->
+      (color = Pc.Black || color = Pc.White) &&
+      (kind = Pc.Amazon || kind = Pc.Arrow)
     end
   ;
   Test.make
     ~name:"empty squares never have pieces on them"
     Arbitrary.coord
-    begin let open Square in
-      fun coord ->
-        (empty coord).piece = None
+    begin fun coord ->
+      (Sq.empty coord).piece = None
     end
   ;
   Test.make
     ~name:"all empty squares can be selected with valid coords on an empty board"
     Arbitrary.coord
     begin fun coord ->
-      Square.is_empty (Board.square Board.empty coord)
+      Sq.is_empty @@ Bd.square Bd.empty coord
     end
   ;
   Test.make
     ~name:"getting a square with invalid coords raises"
     (pair (pair (10 -- 100) (10 -- 100)) Arbitrary.Board.t)
-    begin
-      fun (coord, board) ->
-        match Board.square board coord with
-        | exception Not_found -> true
-        | _ -> false
+    begin fun (coord, board) ->
+      match Bd.square board coord with
+      | exception Not_found -> true
+      | _                   -> false
     end
   ;
   Test.make
@@ -195,11 +193,11 @@ let tests = [
     ~name:"can retrieve piece placed on board"
     Arbitrary.(pair coord Piece.t)
     begin fun (coord, piece) ->
-      let board = Board.empty in
-      match Board.place coord piece board with
+      let board = Bd.empty in
+      match Bd.place coord piece board with
       | Error _  -> false
       | Ok board ->
-        let piece' = Board.square board coord |> Square.piece in
+        let piece' = Bd.square board coord |> Sq.piece in
         piece' = Some piece
     end
   ;
@@ -207,26 +205,25 @@ let tests = [
     ~name:"placing piece on an occupied square returns an Occupied bad_move"
     Arbitrary.(pair coord Piece.t)
     begin fun (coord, piece) ->
-      let board = Aux.place_exn coord piece Board.empty in
-      let sq = Board.square board coord in
-      match Board.place coord piece board with
-      | Ok _ -> false
-      | Error Board.{reason} ->
-        reason = Board.(Occupied sq)
+      let board = Aux.place_exn coord piece Bd.empty in
+      let sq = Bd.square board coord in
+      match Bd.place coord piece board with
+      | Error Bd.{reason} -> reason = Bd.(Occupied sq)
+      | Ok _              -> false
     end
   ;
   Test.make
     ~name:"removing pieces returns correct values"
     Arbitrary.(pair coord Board.t)
     begin fun (coord, board) ->
-      let Square.{piece} as sq = Board.square board coord
+      let Sq.{piece} as sq = Bd.square board coord
       in
-      let piece_is_arrow = Option.exists ~f:Piece.is_arrow piece in
-      let square_was_empty sq' = Square.is_empty sq && Square.is_empty sq' in
+      let piece_is_arrow = Option.exists ~f:Pc.is_arrow piece in
+      let square_was_empty sq' = Sq.is_empty sq && Sq.is_empty sq' in
       let piece_is_removed piece' board' =
-        (Some piece') = piece && Square.is_empty (Board.square board' coord)
+        (Some piece') = piece && Sq.is_empty (Bd.square board' coord)
       in
-      match Board.remove coord board with
+      match Bd.remove coord board with
       | Error {reason = Invalid_piece _ } -> piece_is_arrow
       | Error {reason = Empty sq'}        -> square_was_empty sq'
       | Ok (piece', board')               -> piece_is_removed piece' board'
@@ -239,25 +236,25 @@ let tests = [
     begin fun () ->
       let black_positions = [(6,9) ; (9,6) ; (6,0) ; (9,3)] in
       let white_positions = [(0,6) ; (3,9) ; (0,3) ; (3,0)] in
-      let sq_coords_in coord_list Square.{coord} =
+      let sq_coords_in coord_list Sq.{coord} =
         List.mem ~equal:((=)) coord_list coord in
       let is_black_sq sq = sq_coords_in black_positions sq in
       let is_white_sq sq = sq_coords_in white_positions sq in
-      let has_white_piece sq = Option.exists ~f:Piece.is_white @@ Square.piece sq in
-      let has_black_piece sq = Option.exists ~f:Piece.is_black @@ Square.piece sq in
-      let board = Board.setup in
+      let has_white_piece sq = Option.exists ~f:Pc.is_white @@ Sq.piece sq in
+      let has_black_piece sq = Option.exists ~f:Pc.is_black @@ Sq.piece sq in
+      let board = Bd.setup in
       let (black_squares, rest) = List.partition_tf ~f:is_black_sq board in
       let (white_squares, rest) = List.partition_tf ~f:is_white_sq rest in
       List.for_all white_squares ~f:has_white_piece &&
       List.for_all black_squares ~f:has_black_piece &&
-      List.for_all rest ~f:Square.is_empty
+      List.for_all rest ~f:Sq.is_empty
     end
   ;
   Test.make
     ~name:"line_of_squares only returns squares on a line"
     Arbitrary.(pair coord coord)
     begin fun (source, target) ->
-      match Board.line_of_squares source target Board.empty with
+      match Bd.line_of_squares source target Bd.empty with
       | Some squares -> Aux.are_in_a_line squares
       | None         -> true
     end
@@ -267,11 +264,11 @@ let tests = [
     Arbitrary.(pair Board.t int)
     begin fun (board, n) ->
       let sqs = List.take board n in
-      match Board.only_empty_squares sqs with
+      match Bd.only_empty_squares sqs with
       | Ok squares ->
-        List.for_all squares ~f:Square.is_empty
+        List.for_all squares ~f:Sq.is_empty
       | Error { reason = Blocked occupied_sq } ->
-        not (Square.is_empty occupied_sq)
+        not (Sq.is_empty occupied_sq)
       | _ ->
         false
     end
