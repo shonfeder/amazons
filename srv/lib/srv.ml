be7a1fab6368dec9c9b4614f7bf7fd41bca53377
Shon Feder <shon.feder@key.me>
2018-01-14 00:23:02 -0500

    Basic game-room architecture taking shape

--- srv/lib/srv.ml
+++ srv/lib/srv.ml
@@ -1,6 +1,10 @@
+open Core
 open Opium.Std
 module T = Tyxml
 module H = T.Html
+
+exception Invalid_request of Opium_kernel__Rock.Request.t
+exception Game_room_full
 
 module Page = struct
   let default_css = "/assets/css/amazons.css"
@@ -20,31 +24,88 @@ module Page = struct
              content)
         |> to_string
       end
+
+  let respond ?(title="") ?(css=default_css) content =
+    make ~title ~css content |> respond'
+
+  let page_h1 string = T.Html.(h1 [pcdata string])
+  let game_title game_id =
+    "New Game of the Amazons: #" ^ string_of_int game_id
+
+  let game game game_id =
+    let title = game_title game_id in
+    respond ~title @@
+    [T.Html.div
+       [ page_h1 title
+       ; Render.Html.game game ]]
 end
+
+type game_id = int
+let games : Game.t option array ref =
+  ref (Array.init ~f:(fun _ -> None) 1000)
+
+let create_game () : Game.t * game_id =
+  let new_game = Game.Update.start in
+  let add_new_game game_id =
+    Array.set (!games) game_id (Some new_game) ;
+    (new_game, game_id)
+  in
+  match Array.findi (!games) ~f:(fun _ -> Option.is_none) with
+  | Some (indx, _) -> add_new_game indx
+  | None           -> raise Game_room_full
+
+let lookup_game indx : Game.t option = (!games).(indx)
+
+let new_game_link =
+  T.Html.(a ~a:[a_href "/game/new"] [pcdata "New Game"])
 
 let home = get "/"
     begin fun req ->
       let open T.Html in
-      Page.make [h1 [pcdata "The Game of the Amazons"]]
-      |> respond'
+      Page.respond @@
+      [div
+         [ h1 [pcdata "The Game of the Amazons"]
+         ; ul [li [new_game_link]]
+         ]
+      ]
     end
 
-let game = get "/game"
+let new_game = get "/game/new"
     begin fun req ->
-      [Render.Html.game Game.Update.start]
-      |> Page.make
-      |> respond'
+      let game, game_id = create_game () in
+      let open T.Html in
+      (* TODO redirect to game/:id page *)
+      Page.game game game_id
     end
 
-let move = get "/game/move/:coord"
+let game = get "/game/:id"
     begin fun req ->
-      let coord = param req "coord" |> Json.To.coord in
-      Page.make [H.p [H.pcdata @@
-                      Printf.sprintf
-                        "x is: %i and y is: %i"
-                        coord.x coord.y]]
-      |> respond'
+      let game_id =
+        try param req "id" |> int_of_string
+        with Failure _ -> raise (Invalid_request req)
+      in
+      let open T.Html in
+      match lookup_game game_id with
+        | Some game -> Page.game game game_id
+        | None      -> Page.respond [div [ h1 [pcdata "That game doesn't exist!"]
+                                         ; p  [new_game_link]]]
     end
+
+(* (\* select an amazon for movement *\)
+ * let select_amazon = get "/game/:id/select/:color/:coord"
+ *     begin fun req ->
+ *       let color = match param req "color" |> Game.Piece.color_of_string with
+ *         | Some c -> c
+ *         | None   -> raise (Invalid_request req)
+ *       in
+ *       match square_with_amazon_of_color
+ *       let coord = param req "coord" |> Json.To.coord in
+ *       Page.make [H.p [H.pcdata @@
+ *                       Printf.sprintf
+ *                         "x is: %i and y is: %i"
+ *                         coord.x coord.y]]
+ *       |> respond'
+ *     end *)
 
 let resources = middleware @@
   Middleware.static
@@ -56,5 +117,6 @@ let _ =
   |> resources
   |> home
   |> game
-  |> move
+  |> new_game
+  (* |> select_amazon *)
   |> App.run_command
